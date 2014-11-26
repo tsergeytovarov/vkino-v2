d4136eccadba58a696f4775aa14f2a3e3ca5b686
Сергей <tovarov.piter@gmail.com>
2014-11-27 01:14:02 +0400

    finish main page

--- js/script.js
+++ js/script.js
@@ -9,5 +9,32 @@ $(function(){
 
 		return false;
 	})
+
+
+	$('.slider-carousel').bxSlider({
+		minSlides: 5,
+		maxSlides: 5,
+		slideWidth: 180,
+		slideMargin: 10,
+		moveSlides: 1,
+		pager: false
+	});
+
+	$('.slider-carousel-3').bxSlider({
+		minSlides: 3,
+		maxSlides: 3,
+		slideWidth: 330,
+		slideMargin: 10,
+		moveSlides: 1,
+		pager: false
+	});
+
+	$('.slider-feedback').bxSlider({
+		auto: true,
+		controls: false,
+		pager: false,
+		mode: 'fade',
+		speed: 5
+	});
 	
 });
