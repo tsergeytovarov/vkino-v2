c3685a391d44ae595926f48a3094b39681a51ce0
Сергей <tovarov.piter@gmail.com>
2014-11-26 21:44:02 +0400

    profiles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 $(function(){
 	
-	
+	$('.js-tab-control').on('click', function(){
+		$('.js-tab-control').removeClass('active');
+		$(this).addClass('active');
+
+		$('.js-tab').removeClass('active');
+		$('.' + $(this).attr('data-tab')).addClass('active');
+
+		return false;
+	})
 	
 });
